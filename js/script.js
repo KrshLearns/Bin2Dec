075a68ae3642b1a7369eca8ac532f9d6757a29ed
Noor Mohammad Siddiqui <noors.cseb19@sbjit.edu.in>
2021-10-07 03:07:14 -0400

    Solved #3 | with better dark theme

--- js/script.js
+++ js/script.js
@@ -1,44 +1,128 @@
-const binary = document.getElementById('input-binary')
-const decimal = document.getElementById('input-decimal')
 
-const bin2dec = document.getElementById('btn-binary')
-const dec2bin = document.getElementById('btn-decimal')
-
-const errorbin = document.getElementById('input-error-binary')
-const errordec = document.getElementById('input-error-decimal')
-
-const allowMultiple = document.getElementById('allow-multiple')
-
-bin2dec.onclick = () => {
-    if (!binary.value) {
-        return errorbin.innerText = 'Please enter a value'
+sNames = ["bin", "oct", "dec", "hex"];
+sDigits = ["01", "01234567", "0123456789", "0123456789ABCDEFabcdef"];
+function check(){
+  sfrom=from.value;
+  sto=to.value;
+  if(!checkInp(sDigits[sNames.indexOf(sfrom)], inp.value)){
+    alert("Invalid input");
+  }
+  else{
+    if(sfrom==sto){
+      oup.value=inp.value;
     }
+    else{
+      choose();
+    }
+  }
 
-    binary.value.split('').map((char) => {
-        if (char !== '0' && char !== '1') {
-            return alert('Binary numbers only consists of 0s and 1s')
-        };
-    });
-
-    decimal.value = parseInt(binary.value, 2)
+}
+function checkInp(sys,num){
+  for(i=0; i<num.length; i++){
+    if(sys.indexOf(num[i])==-1){
+      return false;
+    }
+  }
+  return true;
+}
+function choose(){
+  if(sfrom=="bin"){
+    if(sto=="oct") oup.value=binToOct(inp.value);
+    else if(sto=="dec") oup.value=binToDec(inp.value);
+    else if(sto=="hex") oup.value=binToHex(inp.value);
+  }
+  else if(sfrom=="oct"){
+    if(sto=="bin") oup.value=octToBin(inp.value);
+    else if(sto=="dec") oup.value=octToDec(inp.value);
+    else if(sto=="hex") oup.value=octToHex(inp.value);
+  }
+  else if(sfrom=="dec"){
+    if(sto=="bin") oup.value=decToBin(inp.value);
+    else if(sto=="oct") oup.value=decToOct(inp.value);
+    else if(sto=="hex") oup.value=decToHex(inp.value);
+  }
+  else if(sfrom=="hex"){
+    if(sto=="bin") oup.value=hexToBin(inp.value);
+    else if(sto=="oct") oup.value=hexToOct(inp.value);
+    else if(sto=="dec") oup.value=hexToDec(inp.value);
+  }
+  else alert("Error");
 }
 
-dec2bin.onclick = () => {
-    if (!decimal.value) {
-        return errorbin.innerText = 'Please enter a value'
-    }
 
-    let value = decimal.value
-    let bin = 0
-    let rem
-    let i = 1
 
-    while (value != 0) {
-        rem = value % 2;
-        value = parseInt(value / 2);
-        bin = bin + rem * i;
-        i = i * 10;
-    }
-
-    binary.value = bin
+const decToBin = (str) =>
+{ 
+    str  = str * 1;
+    return str.toString(2);
 }
+
+const decToOct = (str) =>
+{ 
+    str  = str * 1;
+    return str.toString(8);
+}
+
+const decToHex = (str) =>
+{ 
+    str  = str * 1;
+    return str.toString(16);
+}
+
+const hexToBin = (str) =>
+{
+    let a = parseInt(str,16);
+    return a.toString(2);
+}
+
+
+const hexToDec = (str) =>
+{
+    return parseInt(str,16);
+}
+
+const hexToOct = (str) =>
+{
+    let a = parseInt(str,16);
+    return a.toString(8);
+}
+
+const binToDec = (str) =>
+{
+    return  parseInt(str,2);
+}
+
+const binToOct = (str) =>
+{
+    let a = parseInt(str,2);
+    return a.toString(8);
+}
+
+const binToHex = (str) =>
+{
+    let a = parseInt(str,2);
+    return a.toString(16);
+}
+
+const octToBin = (str) =>
+{
+    let a = parseInt(str,8);
+    return a.toString(2);
+}
+
+const octToDec = (str) =>
+{
+    return  parseInt(str,8);
+}
+
+const octToHex = (str) =>
+{
+    let a = parseInt(str,8);
+    return a.toString(16);
+}
+
+
+
+
+
+
